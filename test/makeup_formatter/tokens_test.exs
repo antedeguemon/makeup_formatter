b9c3bd0177603e907e38fdc61f87d7f52001788d
antedeguemon <antedeguemon@users.noreply.github.com>
2022-03-01 15:56:21 -0500

    Fix tests

--- test/makeup_formatter/tokens_test.exs
+++ test/makeup_formatter/tokens_test.exs
@@ -2,6 +2,7 @@ defmodule MakeupFormatter.TokensTest do
   use ExUnit.Case, async: true
 
   alias Makeup.Lexers.ElixirLexer
+  alias MakeupFormatter.Fragments.Metadata
   alias MakeupFormatter.Tokens
 
   test "expands tags when a new line is found" do
@@ -10,16 +11,16 @@ defmodule MakeupFormatter.TokensTest do
     fragments = moduledoc |> ElixirLexer.lex() |> Tokens.to_fragments()
 
     assert fragments == [
-             {:line, %{number: 1, visible?: true},
+             {:line, %Metadata{number: 1},
               [
                 {:name_attribute, %{language: :elixir}, "@moduledoc"},
                 {:whitespace, %{language: :elixir}, " "},
                 {:string, %{language: :elixir}, "&quot;&quot;&quot;"}
               ]},
-             {:line, %{number: 2, visible?: true}, [{:string, %{language: :elixir}, "Start"}]},
-             {:line, %{number: 3, visible?: true}, [{:string, %{language: :elixir}, ""}]},
-             {:line, %{number: 4, visible?: true}, [{:string, %{language: :elixir}, "End"}]},
-             {:line, %{number: 5, visible?: true},
+             {:line, %Metadata{number: 2}, [{:string, %{language: :elixir}, "Start"}]},
+             {:line, %Metadata{number: 3}, [{:string, %{language: :elixir}, ""}]},
+             {:line, %Metadata{number: 4}, [{:string, %{language: :elixir}, "End"}]},
+             {:line, %Metadata{number: 5},
               [{:string, %{language: :elixir}, "&quot;&quot;&quot;"}]}
            ]
   end
